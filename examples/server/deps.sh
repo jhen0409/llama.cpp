697f423e571035b94b8e86376b83762a96137390
Jhen <developer@jhen.me>
2023-08-17 19:40:27 +0800

    server : always regenerate asset hpp before compile

--- examples/server/deps.sh
+++ examples/server/deps.sh
@@ -5,14 +5,24 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PUBLIC=$DIR/public
 
-echo "download js bundle files"
-curl https://npm.reversehttp.com/@preact/signals-core,@preact/signals,htm/preact,preact,preact/hooks > $PUBLIC/index.js
-echo >> $PUBLIC/index.js # add newline
+if [ "$1" != "--no-download" ]; then
+  echo "download js bundle files"
+  curl https://npm.reversehttp.com/@preact/signals-core,@preact/signals,htm/preact,preact,preact/hooks > $PUBLIC/index.js
+  echo >> $PUBLIC/index.js # add newline
+fi
 
 FILES=$(ls $PUBLIC)
+UNAME_S=$(uname -s)
 
 for FILE in $FILES; do
   func=$(echo $FILE | tr '.' '_')
   echo "generate $FILE.hpp ($func)"
-  xxd -n $func -i $PUBLIC/$FILE > $DIR/$FILE.hpp
+
+  if [ "$UNAME_S" == "Darwin" ]; then
+    xxd -n $func -i $PUBLIC/$FILE > $DIR/$FILE.hpp
+  elif [ "$UNAME_S" == "Linux" ]; then
+    xxd -i $PUBLIC/$FILE > $DIR/$FILE.hpp
+    replace_prefix="$(echo $PUBLIC | tr '/' '_')_"
+    sed -i "s/$replace_prefix//g" $DIR/$FILE.hpp
+  fi
 done
